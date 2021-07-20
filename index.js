473031ac03264d264f84b3c39fbdcacc1a7bd091
Christopher Cohen <chris@cscohen.com>
2021-07-20 10:11:33 -0600

    commit tree 3e04ac69023bf3ae5cca212662595a62825fa331

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+//function multiply(num1, num2){
+//    return num1 * num2
+//}
+let random = Math.floor(Math.random(num1) * 10) +1
+
+let num3 = 44
+let num4 = 10
+function mod() {
+    return num3 % num4
+}
+const arraySet = [2,3,4,5,10,20]
+function max() {
+    return (Math.max(...arraySet))
+}
+max()
